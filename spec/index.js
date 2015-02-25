4ae25e5e8f701823ef6e6e2e754c8141d50cc1a7
Matt Styles <matt@veryfizzyjelly.com>
2015-02-25 17:44:11 +0000

    #update test all .js in spec

--- spec/index.js
+++ spec/index.js
@@ -1,3 +1,13 @@
 require( 'babel/register' );
 
-require( './single' );
+var fs = require( 'fs' );
+var path = require( 'path' );
+
+// Test every .js file (not this one) in this directory
+var files = fs.readdirSync( __dirname );
+files.forEach( function( filename ) {
+    var filepath = path.resolve( __dirname, filename );
+    if ( fs.statSync( filepath ).isFile() && /\.js/.test( filename ) && !/index/.test( filename ) ) {
+        require( filepath );
+    }
+});
